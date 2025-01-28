ed759d4a63dfc5e9496d5b76513c144d0677e916
Jörg Thalheim <joerg@thalheim.io>
2025-01-28 09:23:16 +0100

    fix indentation

--- flake.nix
+++ flake.nix
@@ -8,37 +8,46 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
-      systems = lib.systems.flakeExposed;
-      perSystem = { inputs', pkgs, system, ... }: {
-	_module.args.pkgs = import inputs.nixpkgs {
-	  inherit system;
-	  config = {
-	    allowUnfree = true;
-	    cudaSupport = true;
-	    # Only for jetson devices: https://en.wikipedia.org/wiki/CUDA#GPUs_supported
-	    # Faster compilation time?
-	    cudaCapabilities = [ "8.7" ];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = lib.systems.flakeExposed;
+        perSystem =
+          {
+            inputs',
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+                cudaSupport = true;
+                # Only for jetson devices: https://en.wikipedia.org/wiki/CUDA#GPUs_supported
+                # Faster compilation time?
+                cudaCapabilities = [ "8.7" ];
 
-	  };
-	  overlays = [ (final: prev: { cudaPackages = inputs'.jetpack-nixos.legacyPackages.cudaPackages; }) ];
-	};
-        packages.default = pkgs.mkShell {
-          packages = [
-            pkgs.bashInteractive
-	    pkgs.python3Packages.torch
-	    pkgs.cudaPackages.cuda_nvcc
-          ];
-          shellHook = ''
-	    export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
-	  '';
-
-	  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-	    "/run/opengl-driver"
-	  ];
-        };
-      };
-    });
+              };
+              overlays = [ (final: prev: { cudaPackages = inputs'.jetpack-nixos.legacyPackages.cudaPackages; }) ];
+            };
+            packages.default = pkgs.mkShell {
+              packages = [
+                pkgs.bashInteractive
+                pkgs.python3Packages.torch
+                pkgs.cudaPackages.cuda_nvcc
+              ];
+              shellHook = ''
+                export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+              '';
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+                "/run/opengl-driver"
+              ];
+            };
+          };
+      }
+    );
 }
-
